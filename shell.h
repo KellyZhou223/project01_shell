b81061c867446fec4d79a8da9af3b7cf99c6f269
sunnyli13 <71572074+sunnyli13@users.noreply.github.com>
2020-12-20 22:20:22 -0500

    fixed a small error

--- shell.h
+++ shell.h
@@ -2,6 +2,6 @@
 #define SHELL_H
 void run_command(char** command);
 void trim_input(char * input);
-void redirect(char** cmd, int** index);
+void redirect(char** cmd, int index);
 int pipe_func(char** cmd, int index);
 #endif
